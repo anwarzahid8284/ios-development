9e56a744e3e975fe7d1585507f0bb3edc08e4254
Anwar Zahid <anwarzahid.bcs@gmail.com>
2025-07-18 11:46:54 -0700

    Initial Commit

---
+++ TestIOS/TestIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestIOSApp.swift
+//  TestIOS
+//
+//  Created by Affan Laptops on 18/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct TestIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TestIOSTests/TestIOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestIOSTests.swift
+//  TestIOSTests
+//
+//  Created by Affan Laptops on 18/07/2025.
+//
+
+import Testing
+@testable import TestIOS
+
+struct TestIOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
