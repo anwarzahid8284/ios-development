46d535d4947c0693256fd21ed168a95489abe406
Anwar Zahid <anwarzahid.bcs@gmail.com>
2025-07-20 11:16:40 -0700

    working VStack ,Text and Image modifier also Builten shape

--- TestIOS/ContentView.swift
+++ TestIOS/ContentView.swift
@@ -3,21 +3,47 @@
 //  TestIOS
 //
 //
+/*  Swift UI
+ 1. VStack (Linear layout vertical orientation)
+ 2. Text and Image  Modifiers
+ 3. Builten Shapes (Rectangle,capsule,cirecle,RectangleRound)
+ */
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        VStack { // vstack is like linear layout with orientatiion vertical
+            Text("Hello, world Hello world Hello world Hello world Hellow !")
+                .font(.largeTitle)
+                .foregroundColor(.blue)
+                .bold()
+                .italic()
+                .lineLimit(2)
+                .background(Color.yellow)
             Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+                .renderingMode(.template)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200, height: 200)
+                .cornerRadius(90)
+                .clipShape(Circle())
+                .background(Color.green)
+                .foregroundStyle(.red)
+            Rectangle()
+                .frame(width: 100,height: 100)
+            Capsule()
+                .frame(width: 100,height: 100)
+            RoundedRectangle(cornerRadius: 20)
+                        .fill(Color.blue)
+                        .frame(width: 200, height: 100)
+                        .shadow(radius: 5)
+            
+            
         }
-        .padding()
+        
     }
 }
-
 #Preview {
     ContentView()
 }
