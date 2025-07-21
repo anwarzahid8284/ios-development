ca3030d87287da2de1236943bce462588ae946d4
Anwar Zahid <anwarzahid.bcs@gmail.com>
2025-07-21 11:46:06 -0700

    done with textField and textEditor

--- TestIOS/ContentView.swift
+++ TestIOS/ContentView.swift
@@ -7,39 +7,40 @@
  1. VStack (Linear layout vertical orientation)
  2. Text and Image  Modifiers
  3. Builten Shapes (Rectangle,capsule,cirecle,RectangleRound)
+ 4. Text Field (for single line text)
+ 5. TextEditor (for multi line text)
  */
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var userName: String = ""
+    @State private var password: String = "password"
     var body: some View {
-        VStack { // vstack is like linear layout with orientatiion vertical
-            Text("Hello, world Hello world Hello world Hello world Hellow !")
-                .font(.largeTitle)
-                .foregroundColor(.blue)
-                .bold()
-                .italic()
-                .lineLimit(2)
-                .background(Color.yellow)
-            Image(systemName: "globe")
-                .renderingMode(.template)
-                .resizable()
-                .scaledToFit()
-                .frame(width: 200, height: 200)
-                .cornerRadius(90)
-                .clipShape(Circle())
-                .background(Color.green)
-                .foregroundStyle(.red)
-            Rectangle()
-                .frame(width: 100,height: 100)
-            Capsule()
-                .frame(width: 100,height: 100)
-            RoundedRectangle(cornerRadius: 20)
-                        .fill(Color.blue)
-                        .frame(width: 200, height: 100)
-                        .shadow(radius: 5)
+        // Text field
+        VStack {
+            TextField("Enter your name",text:$password,onEditingChanged: {
+                _ in print("Editing")
+            })
+                .padding()
+                .textFieldStyle(.roundedBorder)
+                .cornerRadius(10)
+                Text(userName)
+            Button("Done"){
+                
+            }
+        }
+        VStack{
+            TextEditor(text: $userName)
+                .scrollContentBackground(.hidden)
+                .background(Color.gray.opacity(0.3))
+                .multilineTextAlignment(.center)
+                .dynamicTypeSize(.xxxLarge)
+                .lineSpacing(30)
+                .padding()
+                .frame(height: 300)
             
-            
+                
         }
         
     }
--- TestIOS/TestIOSApp.swift
+++ TestIOS/TestIOSApp.swift
@@ -6,7 +6,7 @@
 
 import SwiftUI
 
-@main
+@main // this is the entry point of the app
 struct TestIOSApp: App {
     var body: some Scene {
         WindowGroup {
