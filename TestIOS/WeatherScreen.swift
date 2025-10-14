2c41782f659b890b7b85e0e83cfa3b423708e68c
Anwar Zahid <anwarzahid.bcs@gmail.com>
2025-10-14 03:39:47 -0700

    refactoring of swiftUi code as well  add dark theme

--- TestIOS/WeatherScreen.swift
+++ TestIOS/WeatherScreen.swift
@@ -3,27 +3,13 @@
 import SwiftUI
 
 struct WeatherScreen: View {
+    @State private var isNight = false
     var body: some View {
         ZStack{
-            LinearGradient(gradient: Gradient(colors: [.blue, Color("lightBlue")]), startPoint: .topLeading, endPoint: .bottomTrailing)
-                .edgesIgnoringSafeArea(.all)
+            BackgroundView(isNight: $isNight)
             VStack{
-                Text("Cupertino, CA")
-                    .font(.system(size: 32, weight: .medium, design: .default))
-                    .foregroundColor(.white)
-                    .padding()
-                VStack(spacing:0){
-                    Image(systemName: "cloud.sun.fill")
-                        .renderingMode(.original)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 180, height: 180)
-                    Text("76°")
-                        .font(.system(size: 70, weight: .medium))
-                        .foregroundColor(.white)
-                        
-                }
-                .padding(.bottom,80)
+                CityNameView(cityName: "Cupertino , CA")
+                MainWeatherView(imageName: isNight ? "moon.stars.fill" : "cloud.sun.fill",highTemperature: isNight ? 33 : 76)
                 HStack(spacing: 26){
                     WeatherDayView(dayOfWeek: "TUE", imageName: "cloud.sun.fill", highTemperature: 74)
                     WeatherDayView(dayOfWeek: "WED", imageName: "sun.max.fill", highTemperature: 88)
@@ -34,25 +20,37 @@ struct WeatherScreen: View {
                 Spacer()
                 Spacer()
                 Button(){
-                    print("tapped")
+                    isNight.toggle()
                 }label: {
-                    Text("Change Day Time")
-                        .frame(width: 280, height: 50)
-                        .background(.white)
-                        .font(.system(size: 20, weight: .medium, design: .default))
-                        .cornerRadius(10)
+                    WeatherButtonView(title: "Change Day Time",
+                                      backqroundColor: .white,
+                                      forgoundColor: .blue)
                 }
                 Spacer()
                 
-                    
+                
             }
             
         }
     }
 }
 
-#Preview {
-    WeatherScreen()
+struct BackgroundView: View {
+    @Binding var isNight: Bool
+    
+    var body: some View {
+        LinearGradient(gradient: Gradient(colors: [isNight ? .black : .blue,isNight ? .gray : Color("lightBlue")]), startPoint: .topLeading, endPoint: .bottomTrailing)
+            .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct CityNameView: View {
+    var cityName: String
+    var body: some View {
+        Text(cityName)
+            .font(.system(size: 32, weight: .medium))
+            .foregroundColor(.white)
+    }
 }
 
 struct WeatherDayView: View {
@@ -75,3 +73,43 @@ struct WeatherDayView: View {
         }
     }
 }
+
+struct MainWeatherView: View {
+    var imageName:String
+    var highTemperature:Int
+    var body: some View {
+        VStack(spacing:0){
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 180, height: 180)
+            Text("\(highTemperature)°")
+                .font(.system(size: 70, weight: .medium))
+                .foregroundColor(.white)
+            
+        }
+        .padding(.bottom,80)
+    }
+}
+
+struct WeatherButtonView: View {
+    var title: String
+    var backqroundColor: Color
+    var forgoundColor: Color
+    var body: some View {
+        Text(title)
+            .frame(width: 280, height: 50)
+            .background(backqroundColor)
+            .foregroundColor(forgoundColor)
+            .font(.system(size: 20, weight: .medium, design: .default))
+            .cornerRadius(10)
+    }
+}
+
+
+#Preview {
+    WeatherScreen()
+}
+
+
