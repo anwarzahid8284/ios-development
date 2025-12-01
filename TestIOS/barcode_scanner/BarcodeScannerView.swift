ec3373c87407a8c61ae65cd0f55d4a5b2b6cefc8
Anwar Zahid <anwarzahid.bcs@gmail.com>
2025-12-01 12:49:49 +0500

    add bard code implementation

---
+++ TestIOS/barcode_scanner/BarcodeScannerView.swift
@@ -0,0 +1,18 @@
+//
+//  BarcodeScannerView.swift
+//  TestIOS
+//
+//  Created by Affan Laptops on 01/12/2025.
+//
+
+import SwiftUI
+
+struct BarcodeScannerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BarcodeScannerView()
+}
